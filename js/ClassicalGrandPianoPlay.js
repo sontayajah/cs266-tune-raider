f669143ce15124deec5497576cfe5ba5afc0da20
sontayajah <sontaya.jahm@gmail.com>
2023-01-24 17:57:23 +0700

    Tune Raider: Mini Class Project - CS266

---
+++ js/ClassicalGrandPianoPlay.js
@@ -0,0 +1,322 @@
+document.addEventListener("keypress", test);
+
+function test(e) {
+    keyClick(e.key);
+}
+
+function keyClick(key) {
+    switch (key) {
+        case "1":
+            audio = new Audio("sounds/Classical Grand Piano/1-C2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "!":
+            var audio = new Audio("sounds/Classical Grand Piano/1s-Cs2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "2":
+            var audio = new Audio("sounds/Classical Grand Piano/2-D2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "@":
+            var audio = new Audio("sounds/Classical Grand Piano/2s-Ds2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "3":
+            var audio = new Audio("sounds/Classical Grand Piano/3-E2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "4":
+            var audio = new Audio("sounds/Classical Grand Piano/4-F2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "$":
+            var audio = new Audio("sounds/Classical Grand Piano/4s-Fs2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "5":
+            var audio = new Audio("sounds/Classical Grand Piano/5-G2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "%":
+            var audio = new Audio("sounds/Classical Grand Piano/5s-Gs2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "6":
+            var audio = new Audio("sounds/Classical Grand Piano/6-A2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "^":
+            var audio = new Audio("sounds/Classical Grand Piano/6s-As2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "7":
+            var audio = new Audio("sounds/Classical Grand Piano/7-B2.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "8":
+            var audio = new Audio("sounds/Classical Grand Piano/8-C3.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "*":
+            var audio = new Audio("sounds/Classical Grand Piano/8s-Cs3.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "9":
+            var audio = new Audio("sounds/Classical Grand Piano/9-D3.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "(":
+            var audio = new Audio("sounds/Classical Grand Piano/9s-Ds3.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "0":
+            var audio = new Audio("sounds/Classical Grand Piano/0-E3.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "0":
+            var audio = new Audio("sounds/Classical Grand Piano/0-E3.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "q":
+            var audio = new Audio("sounds/Classical Grand Piano/q.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "Q":
+            var audio = new Audio("sounds/Classical Grand Piano/qQ.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "w":
+            var audio = new Audio("sounds/Classical Grand Piano/w.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "W":
+            var audio = new Audio("sounds/Classical Grand Piano/wW.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "e":
+            var audio = new Audio("sounds/Classical Grand Piano/e.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "E":
+            var audio = new Audio("sounds/Classical Grand Piano/eE.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "r":
+            var audio = new Audio("sounds/Classical Grand Piano/r.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "t":
+            var audio = new Audio("sounds/Classical Grand Piano/t.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "T":
+            var audio = new Audio("sounds/Classical Grand Piano/tT.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "y":
+            var audio = new Audio("sounds/Classical Grand Piano/y.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "Y":
+            var audio = new Audio("sounds/Classical Grand Piano/yY.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "u":
+            var audio = new Audio("sounds/Classical Grand Piano/u.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "i":
+            var audio = new Audio("sounds/Classical Grand Piano/i.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "I":
+            var audio = new Audio("sounds/Classical Grand Piano/iI.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "o":
+            var audio = new Audio("sounds/Classical Grand Piano/o.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "O":
+            var audio = new Audio("sounds/Classical Grand Piano/oO.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "p":
+            var audio = new Audio("sounds/Classical Grand Piano/p.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "P":
+            var audio = new Audio("sounds/Classical Grand Piano/pP.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/Classical Grand Piano/a.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/Classical Grand Piano/s.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "S":
+            var audio = new Audio("sounds/Classical Grand Piano/sS.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/Classical Grand Piano/d.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "D":
+            var audio = new Audio("sounds/Classical Grand Piano/dD.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "f":
+            var audio = new Audio("sounds/Classical Grand Piano/f.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "g":
+            var audio = new Audio("sounds/Classical Grand Piano/g.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "G":
+            var audio = new Audio("sounds/Classical Grand Piano/gG.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "h":
+            var audio = new Audio("sounds/Classical Grand Piano/h.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "H":
+            var audio = new Audio("sounds/Classical Grand Piano/hH.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/Classical Grand Piano/j.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "J":
+            var audio = new Audio("sounds/Classical Grand Piano/jJ.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/Classical Grand Piano/k.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/Classical Grand Piano/l.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "L":
+            var audio = new Audio("sounds/Classical Grand Piano/lL.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "z":
+            var audio = new Audio("sounds/Classical Grand Piano/z.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "Z":
+            var audio = new Audio("sounds/Classical Grand Piano/zZ.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "x":
+            var audio = new Audio("sounds/Classical Grand Piano/x.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "c":
+            var audio = new Audio("sounds/Classical Grand Piano/c.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "C":
+            var audio = new Audio("sounds/Classical Grand Piano/cC.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "v":
+            var audio = new Audio("sounds/Classical Grand Piano/v.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "V":
+            var audio = new Audio("sounds/Classical Grand Piano/vV.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "b":
+            var audio = new Audio("sounds/Classical Grand Piano/b.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "B":
+            var audio = new Audio("sounds/Classical Grand Piano/bB.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "n":
+            var audio = new Audio("sounds/Classical Grand Piano/n.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        case "m":
+            var audio = new Audio("sounds/Classical Grand Piano/m.m4a");
+            audio.volume = 0.1;
+            audio.play();
+            break;
+        default:
+            console.log("sound is not defind");
+    }
+}
